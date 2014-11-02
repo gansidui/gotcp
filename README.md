038ee845c09b852922976dbf672e7ab0cb23a81e
lijie <lijiegm@gmail.com>
2014-11-02 13:15:58 +0800

    Update

--- README.md
+++ README.md
@@ -7,19 +7,20 @@ gotcp is a powerful package for quickly writing tcp applications/services in gol
 Usage
 ================
 
-##Install
+###Install
 
 ~~~
 go get github.com/gansidui/gotcp
 ~~~
 
 
-##Examples
+###Examples
 
 * [Echo](https://github.com/gansidui/gotcp-examples/tree/master/echo)
+* [ChatRoom](https://github.com/gansidui/gotcp-examples/tree/master/chatroom)
 
 
 Document
 ================
 
-[Go Go Go](http://godoc.org/github.com/gansidui/gotcp)
+[GoDoc](http://godoc.org/github.com/gansidui/gotcp)
--- util.go
+++ util.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 )
 
-// Errors
+// Error types
 var (
 	ConnIsClosedError   = errors.New("Conn is closed")
 	WriteIsBlockedError = errors.New("Write packet is blocked")
@@ -13,18 +13,17 @@ var (
 	PacketTooLargeError = errors.New("Packet too large")
 )
 
-// Convert int32 to []byte
+// BigEndian: int32 --> []byte
 func Int32ToBytes(v int32) []byte {
-	buf := make([]byte, 4)
-	buf[0] = byte(v >> 24)
-	buf[1] = byte(v >> 16)
-	buf[2] = byte(v >> 8)
-	buf[3] = byte(v)
-	return buf
+	b := make([]byte, 4)
+	b[0] = byte(v >> 24)
+	b[1] = byte(v >> 16)
+	b[2] = byte(v >> 8)
+	b[3] = byte(v)
+	return b
 }
 
-// Convert []byte to int32
-func BytesToInt32(buf []byte) int32 {
-	v := (int32(buf[0])<<24 | int32(buf[1])<<16 | int32(buf[2])<<8 | int32(buf[3]))
-	return v
+// BigEndian: []byte -->int32
+func BytesToInt32(b []byte) int32 {
+	return int32(b[3]) | int32(b[2])<<8 | int32(b[1])<<16 | int32(b[0])<<24
 }
