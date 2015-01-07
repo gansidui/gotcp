031465ed9c3a13d4ad9440aae516943e6259f14b
jazzthinking <jazzthinking@126.com>
2015-01-07 10:59:37 +0800

    telnet example update

--- examples/telnet/server/server.go
+++ examples/telnet/server/server.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"github.com/gansidui/gotcp"
-	"github.com/gansidui/gotcp/examples/telnet"
+	"fmt"
 	"log"
 	"net"
 	"os"
@@ -10,40 +9,42 @@ import (
 	"runtime"
 	"syscall"
 	"time"
+
+	"github.com/gansidui/gotcp"
+	"github.com/gansidui/gotcp/examples/telnet"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	// listen
+	// create a listener
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", "127.0.0.1:23")
 	checkError(err)
 	listener, err := net.ListenTCP("tcp", tcpAddr)
 	checkError(err)
 
-	// set config and delegate
+	// initialize server params
 	config := &gotcp.Config{
 		AcceptTimeout:          10 * time.Second,
 		ReadTimeout:            60 * 10 * time.Second,
 		WriteTimeout:           60 * 10 * time.Second,
-		MaxPacketLength:        2048,
-		SendPacketChanLimit:    10,
-		ReceivePacketChanLimit: 10,
+		PacketSizeLimit:        2048,
+		PacketSendChanLimit:    10,
+		PacketReceiveChanLimit: 10,
 	}
-	delegate := &telnet.TelnetConnDelegate{}
-	protocol := &telnet.TelnetProtocol{}
+	srv := gotcp.NewServer(config, &telnet.TelnetCallback{}, &telnet.TelnetProtocol{})
 
 	// start server
-	svr := gotcp.NewServer(config, delegate, protocol)
-	go svr.Start(listener)
+	go srv.Start(listener)
+	fmt.Println("listening:", listener.Addr())
 
-	// catch signal
-	ch := make(chan os.Signal)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	log.Printf("Signal: %v\r\n", <-ch)
+	// catch system signal
+	chSig := make(chan os.Signal)
+	signal.Notify(chSig, syscall.SIGINT, syscall.SIGTERM)
+	fmt.Println("Signal: ", <-chSig)
 
 	// stop server
-	svr.Stop()
+	srv.Stop()
 }
 
 func checkError(err error) {
